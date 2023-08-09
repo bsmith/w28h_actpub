9bd1b810cb37e016f5cd5161633245bfc75eac47
Benjamin Smith <bsmith@cabbage.org.uk>
2023-08-09 22:56:25 +0100

    Continuing to explore

--- demo.py
+++ demo.py
@@ -1,21 +1,54 @@
 #!python3
 
 import requests
+import logging
 import json
 
+enable_http_debug = False
+
+if enable_http_debug:
+    # Enabling debugging at http.client level (requests->urllib3->http.client)
+    # you will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+    # the only thing missing will be the response.body which is not logged.
+    try: # for Python 3
+        from http.client import HTTPConnection
+    except ImportError:
+        from httplib import HTTPConnection
+    HTTPConnection.debuglevel = 1
+
+    logging.basicConfig() # you need to initialize logging, otherwise you will not see anything from requests
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+
 # start with a webfinger request
-r = requests.get("https://mikrotik.social/.well-known/webfinger", { "resource": "acct:mikrotik@mikrotik.social" })
+r = requests.get("https://mikrotik.social/.well-known/webfinger", { "resource": "acct:mikrotik@mikrotik.social" }, headers={ "Accept": "application/json" })
 r.raise_for_status()
 webfinger = r.json()
-print(webfinger)
 
 print("got webfinger for %s" % webfinger["subject"])
-actpub_url = None
+person_url = None
 for link in webfinger['links']:
     if link['rel'] == "self" and link['type'] == "application/activity+json":
-        actpub_url = link['href']
+        person_url = link['href']
         break
 
-if actpub_url == None:
+if person_url == None:
     raise Exception("Couldn't find an ActivityPub self link")
-print("Got ActivityPub self link: %s" % actpub_url)
+print("Got ActivityPub self link: %s" % person_url)
+
+# Next get the Person object using this ActivityPub self link
+r = requests.get(person_url, headers={ "Accept": "application/json" })
+r.raise_for_status()
+person = r.json()
+
+print(json.dumps(person, indent=4))
+
+# Let's look in the outbox
+outbox_url = person['outbox']
+print("outbox is %s" % outbox_url)
+r = requests.get(outbox_url, headers={ "Accept": "application/json" })
+r.raise_for_status()
+print(r.text)
